7a5d61c6717e0f6a635fa7d22e7727726cea0b8b
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-05-29 16:11:07 +0530

    added file uploading custom hook and completed upload page

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,7 +1,96 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import { useState, FormEvent, ChangeEvent, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import FileInput from "@/components/FileInput";
+import FormField from "@/components/FormField";
+import { useFileInput } from "@/lib/Hooks/useFileInput";
+import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from "@/constants";
+
+const UploadPage = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [formData, setFormData] = useState<VideoFormValues>({
+    title: "",
+    description: "",
+    tags: "",
+    visibility: "public",
+  });
+  const video = useFileInput(MAX_VIDEO_SIZE);
+  const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE);
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <main className="wrapper-md upload-page">
+      <h1>Upload a video</h1>
+      {error && <div className="error-field">{error}</div>}
+      <form
+        className="rounded-20 gap-6 w-full flex flex-col shadow-10 px-5 py-7.5"
+        onSubmit={() => {}}
+      >
+        <FormField
+          id="title"
+          label="Title"
+          value={formData.title}
+          onChange={handleInputChange}
+          placeholder="Enter a clear and concise video title"
+        />
+
+        <FormField
+          id="description"
+          label="Description"
+          value={formData.description}
+          onChange={handleInputChange}
+          placeholder="Briefly describe what this video is about"
+          as="textarea"
+        />
+
+        <FileInput
+          id="video"
+          label="Video"
+          accept="video/*"
+          file={video.file}
+          previewUrl={video.previewUrl}
+          inputRef={video.inputRef}
+          onChange={video.handleFileChange}
+          onReset={video.resetFile}
+          type="video"
+        />
+
+        <FileInput
+          id="thumbnail"
+          label="Thumbnail"
+          accept="image/*"
+          file={thumbnail.file}
+          previewUrl={thumbnail.previewUrl}
+          inputRef={thumbnail.inputRef}
+          onChange={thumbnail.handleFileChange}
+          onReset={thumbnail.resetFile}
+          type="image"
+        />
+
+        <FormField
+          id="visibility"
+          label="Visibility"
+          value={formData.visibility}
+          onChange={handleInputChange}
+          as="select"
+          options={[
+            { value: "public", label: "Public" },
+            { value: "private", label: "Private" },
+          ]}
+        />
+
+        <button type="submit" className="submit-button">
+          Upload Video
+        </button>
+      </form>
+    </main>
+  );
 };
 
-export default page;
+export default UploadPage;
